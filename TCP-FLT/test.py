bb7ab718d2e0f52d1cbf84f52bc1e29c1d64032b
iulian-042 <56261881+iulian-042@users.noreply.github.com>
2020-06-17 21:30:20 +0300

    Delete test.py

--- TCP-FLT/test.py
+++
@@ -1,8 +0,0 @@
-from PIL import Image
-
-name = "florin.fortis.jpg"
-image = Image.open(name)
-image = image.save("old_" + name)
-image = image.rotate(30, expand=True)
-image = image.convert("L")
-image.save(name)
